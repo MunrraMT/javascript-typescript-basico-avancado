7e11d1409729b4e6c1f465d69ce0907c053ed0eb
André Rodrigues <andre_santos_t@hotmail.com>
2020-09-01 18:34:23 -0400

    Seção 2 - Aula 009

---
+++ secao-02/aula-009/js/index.js
@@ -0,0 +1,6 @@
+let nome = 'João';
+
+console.log(nome, 'nasceu em 1984');
+console.log('Em 2000 ' + nome + ' conheceu Maria');
+console.log('Maria teve 1 filho com', nome, 'em 2015');
+console.log(`O filho de ${nome} se chama Eduardo`);
