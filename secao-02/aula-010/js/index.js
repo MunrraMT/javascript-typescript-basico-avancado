8c1299bd64ad68e5c5a1019693b0af0cfae37beb
André Rodrigues <andre_santos_t@hotmail.com>
2020-09-01 18:53:47 -0400

    Seção 2 - Aula 010

---
+++ secao-02/aula-010/js/index.js
@@ -0,0 +1,14 @@
+const nome = 'João';
+console.log(nome);
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+console.log(resultado);
+const resultadoDuplicado = resultado * 2;
+console.log(resultadoDuplicado);
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+console.log(resultadoTriplicado);
+
+console.log(typeof primeiroNumero);
