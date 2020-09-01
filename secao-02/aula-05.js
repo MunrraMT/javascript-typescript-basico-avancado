8be6775d805e3b7ba115489d394cadaba48aa3e4
André Rodrigues <andre_santos_t@hotmail.com>
2020-09-01 17:47:02 -0400

    Seção 2 - Aula 07

---
+++ secao-02/aula-05.js
@@ -0,0 +1,15 @@
+console.log(
+	`Meu nome é "André Rodrigues". Estou aprendendo JavaScript às ${5} horas da tarde`
+);
+
+console.log(
+	'Meu nome é "André Rodrigues". Estou aprendendo JavaScript às ' +
+		5 +
+		' horas da tarde'
+);
+
+console.log(
+	'Meu nome é "André Rodrigues". Estou aprendendo JavaScript às',
+	20,
+	'da noite'
+);
