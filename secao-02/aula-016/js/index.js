1de9501bb4f864526ab06c748b79a7f0131062b1
André Rodrigues <andre_santos_t@hotmail.com>
2020-09-01 21:43:29 -0400

    Seção 2 - Aula 016

---
+++ secao-02/aula-016/js/index.js
@@ -0,0 +1,24 @@
+// Opedadores Aritméticos
+// + Adição / Concatenação
+// - / *
+// ** Pontenciação
+// % Módulo
+
+const num1 = 10;
+const num2 = 5;
+console.log(num1 ** num2);
+console.log(num1 % num2);
+console.log(num1 + num2 + num1 * num2);
+
+// Ordem de precedencia:
+// ()
+// **
+// * / %
+// + -
+
+// Operadores de Atribuição
+// +=
+// -=
+// *=
+// /=
+// **=
