35358dbe47ede0363bc67563f780cf079bfa7ed9
André Rodrigues <andre_santos_t@hotmail.com>
2020-09-02 15:23:37 -0400

    Seção 2 - Aula 018

---
+++ secao-02/aula-018/js/index.js
@@ -0,0 +1,11 @@
+let num1;
+let num2;
+let resultado;
+
+num1 = Number(prompt('Digite o primeiro número da soma:'));
+
+num2 = Number(prompt('Digite o segundo número da soma:'));
+
+resultado = num1 + num2;
+
+alert(`O resuldado da soma de ${num1} com ${num2} é de ${resultado}`);
