83ae22bcfc919f9487909911e120c4ee99ee28c5
André Rodrigues <andre_santos_t@hotmail.com>
2020-09-10 15:03:01 -0400

    Seção 3 - Aula 035

---
+++ secao-03/aula-035/js/index.js
@@ -0,0 +1,11 @@
+const hora = 24;
+
+if (hora >= 0 && hora <= 11) {
+	console.log('Bom dia');
+} else if (hora >= 12 && hora <= 17) {
+	console.log('Boa tarde');
+} else if (hora >= 18 && hora <= 23) {
+	console.log('Boa noite');
+} else {
+	console.log('Olá!');
+}
