556e8904bbc9122a7eae797ae5432ffe881d3fb9
André Rodrigues <andre_santos_t@hotmail.com>
2020-09-10 13:56:16 -0400

    Seção 3 - Aula 033

---
+++ secao-03/aula-033/js/index.js
@@ -0,0 +1,16 @@
+/*
+OPERADORES LÓGICOS
+
+&& -> AND -> E
+|| -> OR  -> OU
+!  -> NOT -> NÃO
+
+*/
+
+console.log(true && true && true && false && true);
+console.log(true && true && true && true && true);
+
+console.log('');
+
+console.log(true || false || false || false || false);
+console.log(false || false || false || false || false);
