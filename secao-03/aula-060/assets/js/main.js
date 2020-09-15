6af350d6e38959664932814f1e91b6b1775b85fd
André Rodrigues <andre_santos_t@hotmail.com>
2020-09-15 11:52:50 -0400

    Seção 3 - Aula 060

---
+++ secao-03/aula-060/assets/js/main.js
@@ -0,0 +1,20 @@
+function mostraHora() {
+	let data = new Date();
+
+	return data.toLocaleTimeString('pt-BR', {
+		hour12: false,
+	});
+}
+console.log(mostraHora());
+
+const timer = setInterval(function () {
+	console.log(mostraHora());
+}, 1000);
+
+setTimeout(function () {
+	clearInterval(timer);
+}, 3000);
+
+setTimeout(function () {
+	console.log('Olá Mundo');
+}, 5000);
