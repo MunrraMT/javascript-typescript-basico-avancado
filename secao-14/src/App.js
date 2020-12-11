fefd1ced2bf1e8d198db5da1ca6982c593014777
André Rodrigues <dev-asantos@outlook.com>
2020-12-10 23:22:57 -0400

    Seção 14 - Aula 181 - Eslint e Editorconfig

--- secao-14/src/App.js
+++ secao-14/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function app() {
-	return <h1>Hello world!</h1>;
+  return <h1>Hello world!</h1>;
 }
 
 export default app;
