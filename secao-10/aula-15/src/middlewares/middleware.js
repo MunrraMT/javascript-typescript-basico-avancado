60824f8d0cd336cd5e7419ddd69fc49ca631774a
André Rodrigues <dev-asantos@outlook.com>
2020-12-30 10:10:44 -0400

    Seção 10 - Aula 135 - Express Session e Flash Messages

---
+++ secao-10/aula-15/src/middlewares/middleware.js
@@ -0,0 +1,14 @@
+exports.middlewareGlobal = (req, res, next) => {
+  // if (req.body.cliente) {
+  //   console.log();
+  //   console.log(`Vi que você postou ${req.body.cliente}`);
+  //   console.log();
+  // }
+
+  next();
+};
+
+exports.outroMiddleware = (req, res, next) => {
+  console.log('Sou outro middleware');
+  next();
+};
