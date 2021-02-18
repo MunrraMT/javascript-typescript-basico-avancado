808836b0284c3cb4129f510bcfa453a1f1b65c27
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-02-17 21:51:44 -0400

    Aula - 151. Logando o usuário

--- secao-11/projetoagenda/src/controllers/loginController.js
+++ secao-11/projetoagenda/src/controllers/loginController.js
@@ -2,6 +2,7 @@ const Login = require('../models/LoginModel');
 const login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
+  if (req.session.user) return res.render('login-logado');
   res.render('login');
 };
 
@@ -27,4 +28,34 @@ exports.register = async function (req, res) {
     console.log(e);
     res.render('404');
   }
+};
+
+exports.login = async function (req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if (login.errors.length > 0) {
+      req.flash('errors', login.errors);
+      req.session.save(function () {
+        return res.redirect('/login/index');
+      });
+      return;
+    }
+
+    req.flash('success', 'Você entrou no sistema');
+    req.session.user = login.user;
+    req.session.save(function () {
+      return res.redirect('/login/index');
+    });
+    return;
+  } catch (e) {
+    console.log(e);
+    res.render('404');
+  }
+};
+
+exports.logout = function (req, res) {
+  req.session.destroy();
+  res.redirect('/');
 };
