7e8a524dee85eebce60a04d3522f60c19ca07b17
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-08-16 08:49:42 -0400

    Aula 257

---
+++ secao-17/src/A002-type-any/index.ts
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+
+// Utilize ANY em ultimo caso
+function showMessage(msg: any) {
+  return msg;
+}
+
+console.log([1, 3]);
+console.log('Olá');
+console.log(30);
