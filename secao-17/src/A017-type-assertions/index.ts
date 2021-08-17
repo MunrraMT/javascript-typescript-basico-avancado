62a7fb194ad5f0e79c3598a87c67bc365b24524d
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-08-16 23:45:12 -0400

    Aula 272

---
+++ secao-17/src/A017-type-assertions/index.ts
@@ -0,0 +1,19 @@
+const typeAssertions = (): void => {
+  // Condicional
+  const body1 = document.querySelector('body');
+  if (body1) body1.textContent = 'teste';
+
+  // Non-null assertion (!)
+  const body2 = document.querySelector('body')!;
+  body2.textContent = 'teste';
+
+  // Type assertion
+  const body3 = document.querySelector('body') as HTMLBodyElement;
+  body3.textContent = 'teste';
+
+  // HTMLElements
+  const article = document.querySelector('article') as HTMLElement;
+  article.textContent = 'teste2';
+};
+
+typeAssertions();
