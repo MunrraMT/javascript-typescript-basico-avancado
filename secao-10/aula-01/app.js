679a1570596d7c84765fc0c5891de9f62c069338
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2022-03-27 18:16:30 -0400

    124. Módulos (1)

--- secao-10/aula-01/app.js
+++ secao-10/aula-01/app.js
@@ -1,10 +1,17 @@
-const path = require('path');
 const axios = require('axios');
+const { nome, sobrenome, falaNome } = require('./mod1');
 const { Pessoa } = require('./mod1');
 
-const p1 = new Pessoa('Luiz');
+console.log(falaNome());
+
+//
+
+const p1 = new Pessoa('André');
+
 console.log(p1);
 
-// axios('https://www.otaviomiranda.com.br/files/json/pessoas.json')
-//   .then((response) => console.log(response.data))
-//   .catch();
+//
+
+axios('https://www.otaviomiranda.com.br/files/json/pessoas.json')
+  .then((response) => console.log(response))
+  .catch((e) => console.log(e));
--- secao-10/aula-01/mod1.js
+++ secao-10/aula-01/mod1.js
@@ -1,15 +1,32 @@
+const nome = 'Maria';
+const sobrenome = 'Belizário';
+
+const falaNome = () => `${nome} ${sobrenome}`;
+
+console.log(module.exports);
+
+// Maneira longa de exportar variáveis/funções globalmente
+
+module.exports.nome = nome;
+// module.exports.sobrenome = sobrenome;
+// module.exports.falaNome = falaNome;
+
+// Maneira curta de exportar variáveis/funções globalmente
+
+// exports.nome = nome;
+exports.sobrenome = sobrenome;
+exports.falaNome = falaNome;
+
+// Usando This paara exportar variáveis/funções globalmente
+
+this.qualquerCoisa = 'Qualquer Coisaaaa';
+
+console.log(module.exports);
+
 class Pessoa {
   constructor(nome) {
     this.nome = nome;
   }
 }
 
-const nome = 'Luiz';
-const sobrenome = 'Miranda';
-
-// exports.Pessoa = Pessoa;
-module.exports = {
-  nome,
-  sobrenome,
-  Pessoa,
-};
+exports.Pessoa = Pessoa;
